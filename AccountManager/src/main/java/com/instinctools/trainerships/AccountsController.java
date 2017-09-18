486c0e6d1c589337dc171d3eaee0797768ac4c31
RiukSan <riuk95@mail.ru>
2017-09-18 15:43:06 +0300

    Redone project into better state

--- AccountManager/src/main/java/com/instinctools/trainerships/AccountsController.java
+++ AccountManager/src/main/java/com/instinctools/trainerships/AccountsController.java
@@ -2,53 +2,39 @@ package com.instinctools.trainerships;
 
 import java.util.Locale;
 
-import org.hibernate.Session;
-import org.hibernate.Transaction;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.instinctools.trainerships.model.Account;
-import com.instinctools.trainerships.model.Client;
+import com.instinctools.trainerships.services.impl.JPAAccountService;
 
 @Controller
 public class AccountsController {
 
+	ApplicationContext context = new AnnotationConfigApplicationContext(JPAAccountService.class);
+	JPAAccountService accountService = context.getBean(JPAAccountService.class);
+	
 	@RequestMapping(value = "/updateAccount", method = RequestMethod.GET)
 	public String update(Locale locale, Model model, @RequestParam(value = "id") Integer id,
-			@RequestParam(value = "acc_name") String name) {
-		Session session = HibernateUtil.getSessionFactory().openSession();
-		Transaction transaction = session.beginTransaction();
-		Account acc = session.load(Account.class, id);
-		acc.setName(name);
-		session.saveOrUpdate(acc);
-		transaction.commit();
+			@RequestParam(value = "acc_name") String name) {	
+		accountService.update(id, name);
 		return "redirect:/";
 	}
 
 	@RequestMapping(value = "/createAccount", method = RequestMethod.GET)
 	public String create(Locale locale, Model model, @RequestParam(value = "id") Integer id,
 			@RequestParam(value = "acc_name") String name) {
-		Session session = HibernateUtil.getSessionFactory().openSession();
-		Transaction transaction = session.beginTransaction();
-		Client client = session.load(Client.class,id);
-		Account acc = new Account();
-		acc.setName(name);
-		client.addAccount(acc);
-		session.save(client);
-		transaction.commit();
+		accountService.create(id, name);
 		return "redirect:/";
 	}
 
 	@RequestMapping(value = "/deleteAccount", method = RequestMethod.GET)
 	public String delete(Locale locale, Model model, @RequestParam(value = "id") Integer id) {
-		Session session = HibernateUtil.getSessionFactory().openSession();
-		Transaction transaction = session.beginTransaction();
-		Account acc = session.load(Account.class, id);
-		session.delete(acc);
-		transaction.commit();
+		accountService.delete(id);
 		return "redirect:/";
 	}
 
--- AccountManager/src/main/java/com/instinctools/trainerships/services/AccountService.java
+++ AccountManager/src/main/java/com/instinctools/trainerships/services/AccountService.java
@@ -1,48 +1,14 @@
 package com.instinctools.trainerships.services;
 
-import org.hibernate.Session;
-import org.hibernate.Transaction;
 import org.springframework.stereotype.Service;
 
-import com.instinctools.trainerships.HibernateUtil;
 import com.instinctools.trainerships.model.Account;
-import com.instinctools.trainerships.model.Client;
 
 @Service
-public class AccountService {
-	private Session session;
+public interface AccountService {
+	public Account update(Integer id, String name);
 
-	public AccountService() {
-	}
+	public Account create(Integer userId, String name);
 
-	public Account update(Integer id, String name) {
-		session = HibernateUtil.getSessionFactory().openSession();
-		Transaction transaction = session.beginTransaction();
-		Account acc = session.load(Account.class, id);
-		acc.setName(name);
-		session.save(acc);
-		transaction.commit();
-		return acc;
-	}
-
-	public Account create(Integer userId, String name) {
-		session = HibernateUtil.getSessionFactory().openSession();
-		Transaction transaction = session.beginTransaction();
-		Client client = session.load(Client.class, userId);
-		Account acc = new Account();
-		acc.setName(name);
-		client.addAccount(acc);
-		session.save(client);
-		transaction.commit();
-		return acc;
-	}
-
-	public void delete(Integer id) {
-		session = HibernateUtil.getSessionFactory().openSession();
-		Transaction transaction = session.beginTransaction();
-		Account acc = session.load(Account.class, id);
-		session.delete(acc);
-		transaction.commit();
-	}
-
+	public void delete(Integer id);
 }
